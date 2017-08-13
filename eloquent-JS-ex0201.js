5f085fcbc2bb924dd8b13892a21319f57fe4b916
atzzom <atzzom@gmail.com>
2017-08-12 22:13:27 -0500

    Added exercises on chapter 03: functions.

--- eloquent-JS-ex0201.js
+++ eloquent-JS-ex0201.js
@@ -55,4 +55,64 @@ for(var i=1; i<=size; i++){
 }
 console.log(row);
 
+/* --- Exercise 3.2 - even or odd number by recursion --- */
+function isEven(x){
+				  var result;
+					  if(x===0)
+										    return true;
+												  	else if(x===1)
+																		      return false;
+																					  		else if(x>0)
+																												          return isEven(x-2);
+																																	  		else
+																																								          return isEven(x+2);
+}
+
+console.log(isEven(-2));
+
+/* Author's solution */
+function isEven(n) {
+				  if (n == 0)
+									    return true;
+											  else if (n == 1)
+																    return false;
+																		  else if (n < 0)
+																							    return isEven(-n);
+																									  else
+																														    return isEven(n - 2);
+}
+
+/* --- Exercise 3.3 - Bean Counting --- */
+function countChar(line, char){
+					var i = 0;
+					  	var length = line.length;
+							  	var repeated = 0;
+									  	for (i=0; i<=length; i++){
+															    	if (line.charAt(i) === char){
+																						          repeated++;
+																											        }
+																															    }
+																																	  return repeated;
+}
+
+console.log(countChar('Hello World!', 'o'));
+
+/* Author's solution */
+function countChar(string, ch) {
+				  var counted = 0;
+					  for (var i = 0; i < string.length; i++)
+										    if (string.charAt(i) == ch)
+																      counted += 1;
+																			  return counted;
+}
+
+function countBs(string) {
+				  return countChar(string, "B");
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
+
 
