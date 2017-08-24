825b1c9730906680b26e4123943d67b512954837
atzom <atzzom@gmail.com>
2017-08-23 23:19:18 -0500

    Add files via upload

---
+++ ch1_examp2.js
@@ -0,0 +1,5 @@
+/* Script block 1 */
+alert("First Script Block");
+/* Script block 2 */
+document.bgColor = "RED";
+alert("Second Script Block");
---
+++ ch1_examp3.js
@@ -0,0 +1,1 @@
+document.getElementById('ResultsP').innerHTML = 'Hello World!';
