9e42104eea8652c31a05c9d8cefeec472f54f83c
atzom <atzzom@gmail.com>
2017-08-25 22:37:46 -0500

    Examples & exercises from: Beginning JavaScript.
    Files complete the exercises of the 2nd chapter.

---
+++ ch2_exer1.js
@@ -0,0 +1,6 @@
+var degCent = prompt("This page will convert degrees centigrade into degrees Fahrenheit. Please insert the temperature value you would like to convert to \xB0F",25);
+var degFahren;
+
+degFahren = 9/5*degCent + 32;
+
+alert(degCent + "\xB0C is " + degFahren + "\xB0F.");
---
+++ ch2_exer2.js
@@ -0,0 +1,4 @@
+var firstNumber = parseFloat(prompt("Enter the first number",0));
+var secondNumber = parseFloat(prompt("Enter the second number",0));
+var theTotal = firstNumber + secondNumber;
+document.write(firstNumber + " added to " + secondNumber + " equals " + theTotal);
